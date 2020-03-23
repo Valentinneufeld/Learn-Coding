abc924877fa415cd6b4c229b309978dcc5aad683
Valentinneufeld <valentinneufeld@hotmail.de>
2020-03-23 23:11:49 +0100

    add ampPlugin.cpp

---
+++ ampPlugin.cpp
@@ -0,0 +1,27 @@
+
+
+#include "lv2/core/lv2.h"
+
+#include <math.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define AMP_URI "http://example.com"
+
+typedef struct {
+    const float*    gain;
+    const float*    input;
+    float*          output;
+}Amp;
+    
+static LV2_Handle
+instantiate(const LV2_Descriptor*           descriptor,
+            const double                    rate,
+            const char*                     bundle path,
+            const LV2_Feature* const*       feature)
+{
+    Amp* amp = (Amp*)calloc(1, sizeof(Amp));
+    
+    return (LV2_Handle) amp;
+}
+
